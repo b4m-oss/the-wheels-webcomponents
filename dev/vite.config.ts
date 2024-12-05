9ca735ad42a7d61f30a387fa08fc3fb06d4eedea
Kohki SHIKATA <indigoworks@gmail.com>
2024-12-05 18:06:15 +0900

    Update initial settings

--- dev/vite.config.ts
+++ dev/vite.config.ts
@@ -1,6 +1,7 @@
 import { defineConfig } from "vite";
 import vituum from "vituum";
 import nunjucks from "@vituum/vite-plugin-nunjucks";
+import dts from 'vite-plugin-dts';
 
 export default defineConfig({
   plugins: [
@@ -13,12 +14,26 @@ export default defineConfig({
       },
     }),
     nunjucks({
-      root: "./src",
+      root: "./src", // Nunjucksテンプレートのルートディレクトリ
     }),
+    dts(), // TypeScript型定義ファイルの生成
   ],
   build: {
-    rollupOptions: {
-      input: ["./src/pages/**/*.njk"],
+    lib: {
+      entry: 'src/main.ts', // ライブラリのエントリーポイント
+      name: 'TheWheels',
+      fileName: (format) => `my-library.${format}.js`, // 出力ファイル名
     },
+    rollupOptions: {
+      external: ['reset-css'], // 外部依存関係
+      input: ["./src/pages/**/*.njk"], // Nunjucksテンプレートを入力
+      output: {
+        assetFileNames: 'style.css',
+        entryFileNames: '[name].js',
+      },
+    },
+    outDir: 'dist', // 出力ディレクトリ
+    emptyOutDir: true, // ビルド後に出力ディレクトリを空にする
   },
+ 
 });
